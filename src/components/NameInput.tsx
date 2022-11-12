34af6baf2cb6901733ac163576cc681e0c9b01d5
Arturo Dominguez <artdomg@gmail.com>
2022-11-12 02:55:25 -0700

    Allow users joining a game

--- src/components/NameInput.tsx
+++ src/components/NameInput.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react';
-import { useAsyncCallback } from 'react-async-hook';
-import { Button, Form } from 'react-bootstrap';
-import { useGame } from '../context/GameProvider';
-import { FormContainer } from './FormContainer';
+import { useState } from 'react'
+import { useAsyncCallback } from 'react-async-hook'
+import { Button, Form } from 'react-bootstrap'
+import { useGame } from '../context/GameProvider'
+import { joinRoom } from '../services/gameApi'
+import { FormContainer } from './FormContainer'
 
 const NameInput = () => {
-  const { setToken } = useGame();
-  const [username, setUsername] = useState('');
+  const { setToken, room, setMe } = useGame()
+  const [username, setUsername] = useState('')
 
   const onSubmitAsync = useAsyncCallback(async () => {
-    // TODO: Join game
-  });
+    const response = await joinRoom(room, username)
+    const { token, playerId } = response.data
+    setMe(playerId)
+    setToken(token)
+  })
 
   return (
     <FormContainer>
       <Form onSubmit={(e) => e.preventDefault()}>
         <Form.Control
           required
-          type="text"
-          placeholder="Enter name"
+          type='text'
+          placeholder='Enter name'
           value={username}
           onChange={(e) => setUsername(e.currentTarget.value)}
         />
-        <Button type="submit" variant="dark" onClick={onSubmitAsync.execute}>
+        <Button
+          type='submit'
+          variant='dark'
+          onClick={onSubmitAsync.execute}
+          disabled={onSubmitAsync.loading || !username}
+        >
           Join
         </Button>
       </Form>
     </FormContainer>
-  );
-};
+  )
+}
 
-export default NameInput;
+export default NameInput
--- src/services/gameApi.ts
+++ src/services/gameApi.ts
@@ -6,3 +6,6 @@ const instance = axios.create({ baseURL })
 
 export const createRoom = (name: string, password: string) =>
   instance.post('/room', { name, password })
+
+export const joinRoom = (roomId: string, name: string) =>
+  instance.post(`/room/${roomId}/join`, { name })
