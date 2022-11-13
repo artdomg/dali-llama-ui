dde483d019ae2f727f952ff0617644c12545574c
Arturo Dominguez <artdomg@gmail.com>
2022-11-13 09:36:20 -0700

    add sorting to players

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { useGame } from '../context/GameProvider'
+import { Player as PlayerType, useGame } from '../context/GameProvider'
 import Player from './Player'
 
 const PlayerList = () => {
@@ -19,15 +19,25 @@ const PlayerList = () => {
     )
   }, [players, status])
 
+  const sortPlayers = (p1: PlayerType, p2: PlayerType) => {
+    if (p1.score < p2.score) return 1
+    if (p1.score > p2.score) return -1
+    if (p1.name.toLowerCase() > p2.name.toLowerCase()) return 1
+    if (p1.name.toLowerCase() < p2.name.toLowerCase()) return -1
+    return 0
+  }
+
   return (
     <>
-      {Object.values(players).map((player) => (
-        <Player
-          key={player.name}
-          player={player}
-          showMedal={medals[player.id]}
-        />
-      ))}
+      {Object.values(players)
+        .sort(sortPlayers)
+        .map((player) => (
+          <Player
+            key={player.name}
+            player={player}
+            showMedal={medals[player.id]}
+          />
+        ))}
     </>
   )
 }
