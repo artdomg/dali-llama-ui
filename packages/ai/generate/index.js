e8a042c3e5a8e65428c15e83435c7c798afcb650
Arturo Dominguez <artdomg@gmail.com>
2022-11-21 22:42:10 -0700

    Add time

--- packages/ai/generate/index.js
+++ packages/ai/generate/index.js
@@ -3,7 +3,10 @@ const { createPredictions } = require('./replicateApi')
 const main = async (args) => {
   try {
     const prompt = args.prompt
+    const start = Date.now()
     const predictions = await createPredictions(prompt)
+    const time = (Date.now() - start) / 1000
+    console.log({ time })
     return {
       body: {
         prompt,
