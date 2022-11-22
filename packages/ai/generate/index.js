6eb3b937497c03602706a927a0e7699915aac4fb
Arturo Dominguez <artdomg@gmail.com>
2022-11-21 22:11:14 -0700

    try catch

--- packages/ai/generate/index.js
+++ packages/ai/generate/index.js
@@ -1,10 +1,22 @@
 const { createPredictions } = require('./replicateApi')
 
 const main = async (args) => {
-  const prompt = args.prompt
-  console.log('running function')
-  await createPredictions(prompt)
-  return { body: 'Hello World!' }
+  try {
+    const prompt = args.prompt
+    console.log('running function')
+    await createPredictions(prompt)
+    return { body: 'Hello World!' }
+  } catch (err) {
+    console.log(err)
+    return {
+      error: {
+        statusCode: 500,
+        body: {
+          message: 'Unexpected error',
+        },
+      },
+    }
+  }
 }
 
 exports.main = main
